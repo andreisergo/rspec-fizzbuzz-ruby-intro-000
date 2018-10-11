f57d34e1836e81d2ad0ac487fc83f8809bc4255c
Andrei Sergo <a.sergoucht@gmail.com>
2018-10-11 23:20:51 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,13 +4,9 @@ def fizzbuzz(int)
   
   if int % 3 == 0 
     puts "Fizz"
-  end
-  
-  if int % 5 == 0
-    puts "Buzz"
-  end
-  
-  if int % 3 == 0 && int % 5 == 0
-    puts "FizzBuzz"
+    elsif int % 5 == 0
+      puts "Buzz"
+    elsif int % 3 == 0 && int % 5 == 0
+      puts "FizzBuzz"
   end
 end
